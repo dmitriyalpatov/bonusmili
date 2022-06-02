658f521322284263ce128241d40cd1417fac1c37
dmitriyalpatov <dmitriy.alpatov@mail.ru>
2022-06-03 00:09:54 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean rubles_for_one_bonus_milli = true;
+
+        long amaunt_tickets = 15800;
+
+        int percent = 1;
+
+        long bonus = amaunt_tickets * percent / 100 ;
+
+        System.out.println("Ticket price =" + amaunt_tickets);
+
+        System.out.println("Your miles =" + bonus);
+
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: одну для хранения
+        // стоимости билета, другую для хранения количества
+        // рублей для одной бонусной милли
+
+        // Рассчитываете количество бонусных милль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+    }
+}
