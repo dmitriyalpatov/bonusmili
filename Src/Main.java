76adc7165493c17e8e61b0906d6e5032821001b1
Дмитрий <103608303+dmitriyalpatov@users.noreply.github.com>
2022-06-03 00:26:02 +0300

    Update and rename src/Main.java to Src/Main.java

---
+++ Src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 264;
+
+        int theAmountOfBonusMiles = 20;
+
+        int bonusMile = ticketPrice / theAmountOfBonusMiles;
+
+        System.out.println("ticket price " + ticketPrice + " rub., Your bonus " + bonusMile + " bonus miles.");
+    }
+}
